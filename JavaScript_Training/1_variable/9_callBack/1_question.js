1ecca3824d00524096ab1844ae5f0d00f70f1364
ShiotaYoshiaki <ohayoppe-yoshi@outlook.jp>
2020-03-10 22:07:54 +0900

    1_variableの終了

---
+++ JavaScript_Training/1_variable/9_callBack/1_question.js
@@ -0,0 +1,10 @@
+/**
+ * 問題文
+ * callBack関数を使用した処理を記載してください
+ * 
+ * 条件
+ * 1. firstFunc, secondFunk, thirdFuncをそれぞれ作成してください
+ * 2. まず最初にfirstFuncを宣言してください
+ * 3. firstFunc内でsecondFunkを宣言し、引数にthirdFuncを宣言してください
+ * 4. secondFuncでは引数をcallBack関数として実行してください
+ */
