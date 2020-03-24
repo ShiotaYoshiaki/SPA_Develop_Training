46578557c1c3483afee44e0b10ba50471b561200
Hiroki-Fujishima <ucuc.as@gmail.com>
2020-03-24 21:22:51 +0900

    RedButtonのcomponent終了

--- JavaScript_Training/4_React/spa_hiroki/src/App.js
+++ JavaScript_Training/4_React/spa_hiroki/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RedButton from './js/training/components/RedButton';
 
 function App() {
+
+  const domArray = [];
+
+    for (let i = 1; i <= 10; i += 1) {
+      domArray.push(<RedButton param={i}/>)
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        sample
+        {domArray}
+     
+        
         
       </header>
     </div>
+    
+    
   );
+
+
 }
 
 export default App;
---
+++ JavaScript_Training/4_React/spa_hiroki/src/js/training/components/RedButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class RedButton extends React.Component {
+    render() {
+        console.log('=============');
+        console.log(this);
+        console.log('=============');
+        console.log(this.props);
+        return(
+            <button style={{width:'100px',height:'100px',backgroundColor:'red'}}>
+                {this.props.param}
+            </button>
+        );
+     }
+    }
+export default RedButton;
