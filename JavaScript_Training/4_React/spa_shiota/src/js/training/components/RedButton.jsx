62de79755c5109eb1d1490ac0c0714a5c29230a7
ShiotaYoshiaki <ohayoppe-yoshi@outlook.jp>
2020-03-24 21:24:59 +0900

    問題終了まで

---
+++ JavaScript_Training/4_React/spa_shiota/src/js/training/components/RedButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RedButton extends React.Component {
+  render() {
+    return (
+      <button style={{ width: '100px', height: '100px', backgroundColor: 'red' }}>
+        {this.props.param}
+      </button>
+    );
+  }
+}
+
+export default RedButton;
