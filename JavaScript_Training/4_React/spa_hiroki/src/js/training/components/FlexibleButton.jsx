0d3543234064994e9033b083032ec925d46aa059
Hiroki-Fujishima <ucuc.as@gmail.com>
2020-03-26 21:09:54 +0900

    console 削除項目の訂正

--- JavaScript_Training/4_React/spa_hiroki/src/js/training/components/FlexibleButton.jsx
+++ JavaScript_Training/4_React/spa_hiroki/src/js/training/components/FlexibleButton.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 class FlexibleButton extends React.Component {
     render() {
-        console.log('=============');
-        console.log(this);
-        console.log('=============');
-        console.log(this.props);
+      
         return(
             <button style={{width:this.props.width,height:'100px',backgroundColor:'red'}}>
               test
