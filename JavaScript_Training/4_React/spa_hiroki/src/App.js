459c52b92502d7634028a264c8de9d2e2cb42601
Hiroki-Fujishima <ucuc.as@gmail.com>
2020-03-29 21:05:34 +0900

    練習問題４の修正

--- JavaScript_Training/4_React/spa_hiroki/src/App.js
+++ JavaScript_Training/4_React/spa_hiroki/src/App.js
@@ -4,45 +4,25 @@ import './App.css';
 import DifficultButton from './js/training/components/DifficultButton';
 
 function App() {
-
   const domArray = [];
-  
-
-    for (let i = 100, count = 1 ; count <= 10;  i += 20,count += 1) {
-
-     
-
+    for (let i = 100, count = 1 ; count <= 13;  i += 20,count += 1) {
+     let colorParam = 'blue';
+     let Param = count;
       if (count % 3 === 0) {
-
-        domArray.push( <DifficultButton width={i} backgroundColor={'red'} param={count}/>)
-
-       } else if (count % 4 === 0) {
-
-        domArray.push( <DifficultButton width={i} backgroundColor={'blue'} param={'!!'}/>)
-
-       } else {
-
-        domArray.push( <DifficultButton width={i} backgroundColor={'blue'} param={count}/>)
-
-       }
-    
+        colorParam = 'red'
+       } 
+      if (count % 4 === 0) {
+        Param= '!!';
+       } 
+      domArray.push( <DifficultButton width={i} backgroundColor={colorParam} param={Param}/>)
   }
-
-
- 
   return (
     <div className="App">
       <header className="App-header">
       {domArray}
-
-
       </header>
     </div>
-    
-    
   );
-
-
 }
 
 export default App;
