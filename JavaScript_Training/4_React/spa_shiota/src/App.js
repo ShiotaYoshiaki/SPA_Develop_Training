d3093295163d776fe670e7d3ac47c114c4e36a27
ShiotaYoshiaki <ohayoppe-yoshi@outlook.jp>
2020-04-07 20:01:55 +0900

    講座分

--- JavaScript_Training/4_React/spa_shiota/src/App.js
+++ JavaScript_Training/4_React/spa_shiota/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import PlusButton from "./js/calc/containers/pulsButton";
+import CalcResult from "./js/calc/containers/calcResult";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <PlusButton />
+        <CalcResult />
       </header>
     </div>
   );
