7ffe4810d644e9cfb0844d388bd78ad6beaddf79
ShiotaYoshiaki <ohayoppe-yoshi@outlook.jp>
2020-04-09 21:02:23 +0900

    サービスクラスの自訴す

--- JavaScript_Training/4_React/spa_hiroki/src/js/calc/components/calcButton.jsx
+++ JavaScript_Training/4_React/spa_hiroki/src/js/calc/components/calcButton.jsx
@@ -4,7 +4,7 @@ class calcButton extends React.Component {
     render() {
         if (this.props.param === "+") {
             return (
-                <button onClick={this.props.plus}>{this.props.param}</button>);
+                <button onClick={() => this.props.plus(this.props.param)}>{this.props.param}</button>);
         } else if (this.props.param === "-") {
             return (
                 <button onClick={this.props.minus}> {this.props.param} </button>
